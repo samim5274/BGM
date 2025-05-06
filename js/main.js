9c5b39c8b23244a5d433a075a436a779c8560c5c
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2025-05-06 16:55:49 +0600

    Updated

--- js/main.js
+++ js/main.js
@@ -27,31 +27,22 @@
     
     
     // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
+    document.addEventListener("DOMContentLoaded", function () {
+        const toggle = document.getElementById('mega');
+        const menu = document.getElementById('mega-menu');
+
+        toggle.addEventListener('click', function (e) {
+            e.preventDefault();
+            menu.classList.toggle('show');
+        });
+
+        document.addEventListener('click', function (e) {
+            if (!toggle.contains(e.target) && !menu.contains(e.target)) {
+                menu.classList.remove('show');
             }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
+        });
     });
+
     
     
     // Back to top button
